389b87e4d002ece793f691d4e656cdd0f98e33c6
Randal Sean Harrison <rharriso@nd.edu>
2024-04-20 23:28:27 -0400

    removed exposed key

--- js/main.js
+++ js/main.js
@@ -1,29 +1,29 @@
-$(document).ready(function() {
+$(document).ready(function () {
   // Dynamically load the footer -------------------------------------------------
-  $("#navbar").load("navbar.html", function() {
+  $("#navbar").load("navbar.html", function () {
     console.log("Navbar loaded.");
   });
 
   // Dynamically load the footer -------------------------------------------------
-  $("#page-footer").load("footer.html", function() {
+  $("#page-footer").load("footer.html", function () {
     console.log("Footer loaded.");
     $('[data-toggle="tooltip"]').tooltip();
   });
 
   // Link to elements with data-url attributes -----------------------------------
-  $(document).on("click", "[data-url]", function() {
+  $(document).on("click", "[data-url]", function () {
     window.open($(this).attr("data-url"), $(this).attr('data-target'));
   });
 
   // Back to top button ----------------------------------------------------------
-  $(window).scroll(function() {
+  $(window).scroll(function () {
     if ($(this).scrollTop() > 100) {
       $('#back-to-top').fadeIn("fast");
     } else {
       $('#back-to-top').fadeOut("fast");
     }
   });
-  $('#back-to-top').click(function() {
+  $('#back-to-top').click(function () {
     $("html, body").animate({
       scrollTop: 0
     }, 400);
@@ -33,13 +33,13 @@ $(document).ready(function() {
   // South Bend Time function ----------------------------------------------------
   // Time in South Bend -- thanks to Rob Parham @pamblam (Github)
   $("#south-bend-time").remoteTime({
-    key: "AIzaSyDSDJ7_CO-v3xr8nIOtBUXvw_I_PpDTO8A",
+    key: "",
     location: "South Bend, Indiana",
     format: "g:i a" // removed seconds from format: "g:i:s a"
   });
 
   // reset-adjacent-input-field -------------------------------------------------
-  $(".reset-adjacent-input-field").on("click", function() {
+  $(".reset-adjacent-input-field").on("click", function () {
     $(this).parent().parent().parent().find("input").val("");
   });
 
